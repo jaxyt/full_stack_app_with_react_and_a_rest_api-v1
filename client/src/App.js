f259e4a33be275e4aa9e27afde5051434383351e
= <jax.maximillion.taylor@gmail.com>
2019-08-21 03:08:55 -0400

    basic setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+// global components
+import Header from './components/Header';
+
+// route components
+import Public from './components/Public';
+import NotFound from './components/NotFound';
+import UserSignIn from './components/UserSignIn';
+import UserSignUp from './components/UserSignUp';
+import UserSignOut from './components/UserSignOut';
+import Authenticated from './components/Authenticated';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Public} />
+          <Route path="/authenticated" component={Authenticated} />
+          <Route path='/signin' component={UserSignIn} />
+          <Route path='/signup' component={UserSignUp} />
+          <Route path='/signout' component={UserSignOut} />
+          <Route component={NotFound} />
+        </Switch>
+      </div> 
+    </Router>
+
   );
 }
 
---
+++ client/src/components/Public.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+export default class Course extends Component {
+    render() {
+        return (
+            <div className="bounds">
+                <div className="grid-100">
+                    <h1>Welcome to the Main Page</h1>
+                </div>
+            </div>
+        );
+    }
+}
