bee714ce56894e8927b7dcdb1374068c19db5e6e
= <jax.maximillion.taylor@gmail.com>
2019-09-15 15:34:36 -0400

    ready for submission

--- client/src/App.js
+++ client/src/App.js
@@ -35,21 +35,20 @@ export default () => (
   <Router>
     <div>
       <HeaderWithContext />
-      <ErrorBoundary>
-        <Switch>
-          <Route exact path="/" component={CoursesWithContext} />
-          <PrivateRoute path="/authenticated" component={AuthWithContext} />
-          <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
-          <PrivateRoute path="/courses/:id/update" component={UpdateCourseWithContext} />
-          <Route path="/courses/:id" component={CourseDetailWithContext} />
-          <Route path="/signin" component={UserSignInWithContext} />
-          <Route path="/signup" component={UserSignUpWithContext} />
-          <Route path="/signout" component={UserSignOutWithContext} />
-          <Route path="/forbidden" component={Forbidden} />
-          <Route path="/error" component={UnhandledError} />
-          <Route component={NotFound} />
-        </Switch>
-      </ErrorBoundary>
+      
+      <Switch>
+        <Route exact path="/" component={CoursesWithContext} />
+        <PrivateRoute path="/authenticated" component={AuthWithContext} />
+        <PrivateRoute path="/courses/create" component={CreateCourseWithContext} />
+        <PrivateRoute path="/courses/:id/update" component={UpdateCourseWithContext} />
+        <Route path="/courses/:id" component={CourseDetailWithContext} />
+        <Route path="/signin" component={UserSignInWithContext} />
+        <Route path="/signup" component={UserSignUpWithContext} />
+        <Route path="/signout" component={UserSignOutWithContext} />
+        <Route path="/forbidden" component={Forbidden} />
+        <Route path="/error" component={UnhandledError} />
+        <Route component={NotFound} />
+      </Switch>
       
     </div>
   </Router>
